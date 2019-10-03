5fe1b452ab5a0cfb4996e214881e18efe91849f3
Coleman Broaddus <broaddus@mpi-cbg.de>
2019-10-03 17:30:04 +0200

    removed sigma arg and added sigma optimization funcs.

--- src/nlm_comparison.py
+++ src/nlm_comparison.py
@@ -12,7 +12,7 @@ def imsave(x, name, **kwargs): return tifffile.imsave(str(name), x, **kwargs)
 def imread(name,**kwargs): return tifffile.imread(str(name), **kwargs)
 
 
-def nlm_2d(rawdata, savedir, sigma=0.1, **kwargs):
+def nlm_2d(rawdata, savedir, **kwargs):
   # dir = "nlm"
   # savedir = denoise_experiments / f'flower/e01/{dir}/'
   savedir = Path(savedir)
@@ -25,6 +25,7 @@ def nlm_2d(rawdata, savedir, sigma=0.1, **kwargs):
   for x in img:
     ## gputools.denoise.nlm2(data, sigma, size_filter=2, size_search=3)
     ## for noise level of sigma_0, choose sigma = 1.5*sigma_0
+    sigma = 0.1826499502297115 ## obtained through optimization vs GT
     x = gputools.denoise.nlm2(x,sigma,**kwargs)
     pimg.append(x)
   pimg = np.array(pimg)
@@ -46,7 +47,50 @@ def nlm_3d_cele(savedir, sigma=0.1, **kwargs):
     pimg = gputools.denoise.nlm3(img,sigma,**kwargs)
     imsave(pimg, savedir / f'denoised{i:03d}.tif')
 
-def bm3d_2d(rawdata, savedir, sigma=0.1, **kwargs):
+from scipy.optimize import minimize_scalar
+flowerdata = '/lustre/projects/project-broaddus/rawdata/artifacts/flower.tif'
+flowerdir  = '/lustre/projects/project-broaddus/denoise_experiments/flower/e01/'
+
+def optimize_bm3d():
+  img = imread(flowerdata)
+  img = normalize3(img,2,99.6)
+  gt = img.mean(0)
+  bm3d = "/projects/project-broaddus/comparison_methods/bm3d/build/bm3d"
+  tmp = flowerdir + 'bm3d/eg0.tif'
+  imsave(img[0], tmp)
+  outname = flowerdir + 'bm3d/res0.tif'
+
+  def obj(sigma):
+    run(f"{bm3d} {tmp} {sigma} {outname}",shell=True)
+    res = imread(outname)
+    return ((gt-res)**2).mean()
+
+  print(minimize_scalar(obj, bracket=(0.01, 0.3, 0.5)))
+
+def optimize_nlm():
+  img = imread(flowerdata)
+  img = normalize3(img,2,99.6)
+  gt = img.mean(0)
+
+  # bm3d = "/projects/project-broaddus/comparison_methods/bm3d/build/bm3d"
+  # tmp = flowerdir + 'nlm/eg0.tif'
+  # imsave(img[0], tmp)
+  # outname = flowerdir + 'nlm/res0.tif'
+
+  def obj(sigma):
+    # run(f"{bm3d} {tmp} {sigma} {outname}",shell=True)
+    # res = imread(outname)
+    res = gputools.denoise.nlm2(img[1],sigma)
+    return ((gt-res)**2).mean()
+
+  # for s in [0.01,0.1,0.5,0.9,2.0]:
+  #   print(obj(s))
+
+  print(minimize_scalar(obj, bracket=(0.1,0.5,0.9)))
+
+
+
+def bm3d_2d(rawdata, savedir, **kwargs):
   img = imread(rawdata)
   img = normalize3(img,2,99.6)
 
@@ -54,10 +98,13 @@ def bm3d_2d(rawdata, savedir, sigma=0.1, **kwargs):
   savedir = Path(savedir); savedir.mkdir(exist_ok=True,parents=True)
   tmpdir = savedir / 'tmp/'; tmpdir.mkdir(exist_ok=True,parents=True)
 
+  sigma = 0.15488 ## optimized vs GT
+
   for i in range(100):
     tmpname = tmpdir  / f"img{i:03d}.tif"
     outname = savedir / f"img{i:03d}.tif"
 
     if not tmpname.exists():
       imsave(img[i], tmpname)
-    run(f"{bm3d} {tmpname} 0.1 {outname}",shell=True)
+    run(f"{bm3d} {tmpname} {sigma} {outname}",shell=True)
+
